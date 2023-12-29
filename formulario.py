44479158761eb59660f44c28ae46c1c55f5c882b
Pedro <pg100526@gmail.com>
2023-12-29 19:35:34 -0300

    terceiro commit

--- formulario.py
+++ formulario.py
@@ -2,16 +2,16 @@ from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.get('/')
 def index():
     return render_template('index.html')
     
     
-@app.route('/teste')
+@app.get('/teste')
 
 def teste():
     return render_template('m.html')
     
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
